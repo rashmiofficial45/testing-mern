ea98748182c16b20153ab1430f555b9b742d8cbb
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2026-02-12 01:56:00 +0530

    fixed the prisma client error.

--- express-prisma-vitest/src/db.ts
+++ express-prisma-vitest/src/db.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prismaClient = new PrismaClient();
+// 1. Create a PostgreSQL connection pool
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+
+// 2. Create the Prisma adapter
+const adapter = new PrismaPg(pool);
+
+// 3. Pass the adapter to PrismaClient
+export const prismaClient = new PrismaClient({ adapter });
